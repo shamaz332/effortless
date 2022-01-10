5a0507797bda88caa14b038c17a3cf9559d49e40
Shamaz saeed <shamazsaeed332@gmail.com>
2022-01-11 01:21:26 +0500

    register page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CoinDetails from "./components/CoinDetails";
 import Footer from "./components/Footer";
 import Home from "./views/home";
 import Navbar from './components/Navbar'
+import Register from "./components/Register";
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
 <Routes>
         <Route path="/" element={<Home />} />
         <Route path="coindetail" element={<CoinDetails />} />
+        <Route path="register" element={<Register />} />
+
       </Routes>
       <Footer/>
     </div>
---
+++ src/components/Register.js
@@ -0,0 +1,58 @@
+import React from "react";
+import wallet from "../assests/wallet.svg";
+
+const Register = () => {
+  return (
+    <div className="flex flex-wrap justify-center items-center p-4">
+      <div className="lg:w-2/5 sm:w-full flex flex-col items-center">
+        <img src={wallet} className="w-64" />
+        <p className="text_color lg:text-3xl sm:text-sm">New Here ?</p>
+        <p className="text_color px-20 text-center">
+          Welcome to DeFi! Create a crypto wallet to start trading on Matcha.
+        </p>
+        <button
+          type="button"
+          class="m-2 get_started w-40 rounded-md text-center py-2 bg-white text-xl font-medium text_color "
+          id="menu-button"
+          aria-expanded="true"
+          aria-haspopup="true"
+        >
+          Get Started
+        </button>{" "}
+      </div>
+      <div className="border-l-2 hidden lg:block flex justify-center text-center h-64 text_color"></div>
+
+      <div className="lg:w-2/5 sm:w-full flex flex-col justify-center items-center">
+        <button
+          type="button"
+          class="m-2 register_btn w-full rounded-md text-center py-2 bg-white text-xl font-medium text_color "
+          id="menu-button"
+          aria-expanded="true"
+          aria-haspopup="true"
+        >
+          Connect mobile wallet{" "}
+        </button>{" "}
+        <button
+          type="button"
+          class="m-2 register_btn w-full rounded-md text-center py-2 bg-white text-xl font-medium text_color "
+          id="menu-button"
+          aria-expanded="true"
+          aria-haspopup="true"
+        >
+          Connect colnebase wallet{" "}
+        </button>{" "}
+        <button
+          type="button"
+          class="m-2 register_btn w-full rounded-md text-center py-2 bg-white text-xl font-medium text_color "
+          id="menu-button"
+          aria-expanded="true"
+          aria-haspopup="true"
+        >
+       Connect Biskti wallet{" "}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
