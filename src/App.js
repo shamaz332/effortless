99880b8710ff4ba33d2b50922150e56fca05629e
Shamaz saeed <shamazsaeed332@gmail.com>
2022-01-19 07:55:54 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import ArgantMobile from "./components/Setup/ArgantMobile";
 import BinancePath from "./components/Setup/BinancePath";
