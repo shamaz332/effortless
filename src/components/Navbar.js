4d380fe8e6d9b8ac406be879d0f1e14d4bf45353
Shamaz saeed <shamazsaeed332@gmail.com>
2022-04-13 18:20:32 +0500

    changes done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,9 @@ import { Menu, Transition } from "@headlessui/react";
 import { MenuIcon, UserCircleIcon } from "@heroicons/react/solid";
 import React, { Fragment, useState } from "react";
 
+import homeIcom from "../assests/home_icon.svg"
+import tradeIcon from "../assests/trade icon 1.svg"
+
 function Nav() {
   const [isOpen, setIsOpen] = useState(false);
   return (
@@ -55,20 +58,7 @@ function Nav() {
                 })}
               >
                 <button className="p-2 nav_btn active inline-flex items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                    />
-                  </svg>
+                 <img src={homeIcom} className="w-5 mr-1"/>
                   <span>Home</span>
                 </button>
               </NavLink>
@@ -84,20 +74,7 @@ function Nav() {
             })}
           >
                 <button className="p-2 nav_btn inline-flex items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4"
-                    />
-                  </svg>
+                <img src={tradeIcon} className="w-5 mr-1"/>
                   <span>Trade</span>
                 </button>
               </NavLink>
